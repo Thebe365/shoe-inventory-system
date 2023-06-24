2c88b43f4641c0a727d2cad4107b472ca3457a92
Lesego Matsimela <mmatismbale@outlook.com>
2023-06-24 10:42:25 +0200

    initial backend commit

---
+++ src/main/java/com/team4/IMS/ImsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.team4.IMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImsBackendApplication.class, args);
+	}
+
+}
