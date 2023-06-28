b975294dfe1040a57c3fa0c9604cab796f3151a1
Lesego Matsimela <lesegomat@harambee.co.za>
2023-06-28 09:43:15 +0200

    end-no-edit

--- backend/src/main/java/com/team4/IMS/Configs/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/team4/IMS/Configs/JwtAuthenticationFilter.java
@@ -39,7 +39,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         final String userEmail;
         final JwtService jwtService;
 
-
         if(request.getRequestURI().equals("/api/v1/auth/authenticate") || request.getRequestURI().equals("/api/v1/shoes")){
             System.out.println("request.getRequestURI().equals(\"/api/v1/auth/authenticate\")");
             filterChain.doFilter(request, response);
