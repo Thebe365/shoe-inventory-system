e55257cb13776464ce9b7c6936bb6de36b44c99f
Nimbus3020 <kthuthukani4@gmail.com>
2023-07-10 11:17:21 +0200

    Customer services 2

---
+++ backend/src/main/java/com/team4/ims/Repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.team4.IMS.repository;
+
+import com.team4.IMS.Models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByEmail(String email);
+
+}
