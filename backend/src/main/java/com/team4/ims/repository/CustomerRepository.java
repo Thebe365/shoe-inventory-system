57fc4318d597c6fe758ca1c8370018137b75d3a3
lesegomat <mmatismbale@outlook.com>
2023-07-10 12:03:49 +0200

    one commit on yourBranch

---
+++ backend/src/main/java/com/team4/ims/repository/CustomerRepository.java
@@ -0,0 +1,15 @@
+package com.team4.ims.repository;
+
+import com.team4.ims.Models.Shoe;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Shoe, Long> {
+    /**TODO: Implement CustomerRepository:
+     *  1. Fetch available shoes for a customer ignore
+     *  2. Fetch shoes of a specific color brand and size (Still under discussion)
+     *  3. Purchase shoes and update the stock
+     *  4. Filter by brand, color, name
+     * */
+}
