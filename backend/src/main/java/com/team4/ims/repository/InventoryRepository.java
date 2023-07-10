df5f5c4c85f26fc7c79510b098e95fbee781dfeb
Nimbus3020 <kthuthukani4@gmail.com>
2023-07-10 11:06:59 +0200

    Customer services

--- backend/src/main/java/com/team4/ims/repository/InventoryRepository.java
+++ backend/src/main/java/com/team4/ims/repository/InventoryRepository.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package com.team4.ims.repository;
 
 import com.team4.ims.Models.Inventory;
@@ -10,5 +11,14 @@ public interface InventoryRepository extends JpaRepository<Inventory, Long> {
     Inventory findInventoryByShoeIdAndColorAndSize(Long id, String color, String size);
 
 
+=======
+package com.team4.IMS.repository;
+
+import com.team4.IMS.Models.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    Inventory findInventoryByShoeId(Long id);
+>>>>>>> cfaba24 (Customer services)
 
 }
