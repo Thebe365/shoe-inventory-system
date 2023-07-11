814f0ad850c266f59acecea5272acd129a99a66b
lesegomat <mmatismbale@outlook.com>
2023-07-11 10:39:05 +0200

    fixing add shoe functionality

--- backend/src/main/java/com/team4/IMS/Models/Shoe.java
+++ backend/src/main/java/com/team4/IMS/Models/Shoe.java
@@ -1,11 +1,8 @@
 package com.team4.ims.Models;
 
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
-
-import java.util.List;
 
 @Data
 @Builder
@@ -32,9 +29,9 @@ public class Shoe implements java.io.Serializable{
     @Column(name = "is_available")
     private Boolean isAvailable;
 
-    @OneToMany(mappedBy = "shoe")
-    @JsonIgnore
-    private List<Inventory> inventory;
+//    @OneToMany(mappedBy = "shoe")
+//    @JsonIgnore
+//    private List<Inventory> inventory;
 
 
 }
