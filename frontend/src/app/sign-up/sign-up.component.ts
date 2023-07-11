a02f05aa729799522f4efd1a37574e8ac1f23083
thebe <Thebe@digilink.africa>
2023-07-11 10:34:46 +0200

    user sign up

---
+++ frontend/src/app/sign-up/sign-up.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2'; 
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css']
+})
+export class SignUpComponent {
+
+  loginForm: FormGroup;
+
+
+  formSubmit(){
+    
+  }
+}
